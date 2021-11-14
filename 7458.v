5ded914b40f03d4ede168a33b03e349407c75c0e
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2021-11-14 15:44:11 +0800

    Update 7458.v

--- 7458.v
+++ 7458.v
@@ -9,7 +9,7 @@ module top_module (
     // assign p2y = (p2a & p2b) | (p2c & p2d);
     /* Method 1 */
     
-	/* Method 2 */
+    /* Method 2 */
     wire p1signal1 = p1a & p1b & p1c;
     wire p1signal2 = p1d & p1e & p1f;
     assign p1y = p1signal1 | p1signal2;
