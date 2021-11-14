09e48dd8d7f22f3cad16eb9b2b9e03cc7e05c7ae
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2021-11-14 16:39:18 +0800

    Create wire4.v

---
+++ wire4.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+
+    // assign output_wire = input_wire;
+    assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+    
+endmodule
