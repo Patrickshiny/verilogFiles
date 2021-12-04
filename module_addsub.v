5caa8cbe675a7c1feea7129a37e794b894510c2c
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2021-12-05 00:01:46 +0800

    Update module_addsub.v

--- module_addsub.v
+++ module_addsub.v
@@ -11,8 +11,8 @@ module top_module(
 );
     wire [31:0] xorResult;
     wire internalWire1, internalWire2;
-    assign xorResult = b xor {32{sub}};
+    assign xorResult = b ^ {32{sub}};
     add16 add16Module1(.a(a[15:0]), .b(xorResult[15:0]), .cin(sub), .cout(internalWire1), .sum(sum[15:0]));
-    add16 add16Module2(.a(a[31:16]), b(xorResult[31:16]), .cin(internalWire1), .cout(internalWire2), .sum(sum[31:16]));
+    add16 add16Module2(.a(a[31:16]), .b(xorResult[31:16]), .cin(internalWire1), .cout(internalWire2), .sum(sum[31:16]));
     
 endmodule
